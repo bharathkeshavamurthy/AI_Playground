791045fc51675ac946de240405ba3977b2ebf6f3
Bharath Keshavamurthy <bkeshava@cisco.com>
2019-06-30 21:10:43 -0700

    AdaptiveBufferIntelligence - [WIP]

--- src/main/python/com/cisco/ai/playground/AdaptiveBufferIntelligence.py
+++ src/main/python/com/cisco/ai/playground/AdaptiveBufferIntelligence.py
@@ -5,3 +5,17 @@
 # Author: Bharath Keshavamurthy
 # Organization: CISCO Systems, Inc.
 # Copyright (c) 2019. All Rights Reserved.
+
+
+# The CISCO Nexus DC switch environment
+# Definitions of states, actions, rewards, transitions, emissions, and steady-state initializations are encapsulated...
+# ...within this class.
+class Nexus(object):
+
+    # The initialization sequence
+    def __init__(self):
+        print('[INFO] Nexus Initialization: Bringing things up...')
+
+    # The termination sequence
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print('[INFO] Nexus Termination: Tearing things down...')
